4e063afa758ba39e63beb41211781b659996dfa6
Alan MacLeod <amacleod@rskorbital.com>
2017-07-09 16:57:58 +0700

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// Add geometry to scene
+
+// Add start point node
+
+// Add endpoint node
+
+// Line of sight test endpoint -> other endpoints (optimise! otherwise O(N2))
+
+// Create scene graph from vis endpoints
+
+// Use Dijkstra or A* to find shortest route
