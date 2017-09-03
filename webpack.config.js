62282ac14d81999122bc880899be0cae8c7afb82
Alan MacLeod <amacleod@rskorbital.com>
2017-09-03 16:35:40 +0700

    webpack cfg

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+
+var path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname,'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
