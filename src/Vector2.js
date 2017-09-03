cc1fae6a321c16958210af549ca12f158cf488e6
Alan MacLeod <amacleod@rskorbital.com>
2017-09-03 16:35:24 +0700

    vector type for convenience

---
+++ src/Vector2.js
@@ -0,0 +1,173 @@
+
+class Vec2
+{
+  add(a, b)
+  {
+    return [
+      a[0] + b[0],
+      a[1] + b[1]
+    ]
+  }
+
+  sub(a, b)
+  {
+    return [
+      a[0] - b[0],
+      a[1] - b[1]
+    ];
+  }
+
+  norm(v)
+  {
+    let m = this.mag( v );
+    return m===0 ? [0,0] : [v[0]/m, v[1]/m];
+  }
+
+  mag(v)
+  {
+    return Math.sqrt(v[0] * v[0] + v[1]*v[1]);
+  }
+
+  intersect(a, b, c, d)
+  {
+    return intersects(a, b, c, d);
+  }
+
+
+}
+
+let Vector2 = new Vec2();
+console.log("Vector2 = ", Vector2.intersect)
+export default Vector2;
+
+
+/**
+ * @author Peter Kelley
+ * @author pgkelley4@gmail.com
+ */
+/**
+ * See if two line segments intersect. This uses the
+ * vector cross product approach described below:
+ * http://stackoverflow.com/a/565282/786339
+ *
+ * @param {Object} p point object with x and y coordinates
+ *  representing the start of the 1st line.
+ * @param {Object} p2 point object with x and y coordinates
+ *  representing the end of the 1st line.
+ * @param {Object} q point object with x and y coordinates
+ *  representing the start of the 2nd line.
+ * @param {Object} q2 point object with x and y coordinates
+ *  representing the end of the 2nd line.
+ */
+
+function intersects(ap, ap2, aq, aq2)
+{
+  // AM: Note to developers, using named properties for vectors is retarded. thanks.
+  return doLineSegmentsIntersect( {x: ap[0], y: ap[1]}, {x: ap2[0], y: ap2[1]},
+                                  {x: aq[0], y: aq[1]}, {x: aq2[0], y: aq2[1]} );
+}
+
+function doLineSegmentsIntersect(p, p2, q, q2)
+{
+
+  // var debug_string = `doLineSegmentsIntersect: (${p.x}, ${p.y})-(${p2.x}, ${p2.y})  with  (${q.x}, ${q.y})-(${q2.x}, ${q2.y})`;
+
+	var r = subtractPoints(p2, p);
+	var s = subtractPoints(q2, q);
+
+	var uNumerator = crossProduct(subtractPoints(q, p), r);
+	var denominator = crossProduct(r, s);
+
+	if (uNumerator == 0 && denominator == 0) {
+		// They are coLlinear
+
+		// Do they touch? (Are any of the points equal?)
+		if (equalPoints(p, q) || equalPoints(p, q2) || equalPoints(p2, q) || equalPoints(p2, q2)) {
+			return true
+		}
+		// Do they overlap? (Are all the point differences in either direction the same sign)
+		return !allEqual(
+				(q.x - p.x < 0),
+				(q.x - p2.x < 0),
+				(q2.x - p.x < 0),
+				(q2.x - p2.x < 0)) ||
+			!allEqual(
+				(q.y - p.y < 0),
+				(q.y - p2.y < 0),
+				(q2.y - p.y < 0),
+				(q2.y - p2.y < 0));
+	}
+
+	if (denominator == 0) {
+		// lines are paralell
+		return false;
+	}
+
+	var u = uNumerator / denominator;
+	var t = crossProduct(subtractPoints(q, p), s) / denominator;
+
+  // let res =
+  return (t >= 0) && (t <= 1) && (u >= 0) && (u <= 1);
+
+  // console.log(`${debug_string} = ${res}`);
+
+	// return res;
+}
+
+/**
+ * Calculate the cross product of the two points.
+ *
+ * @param {Object} point1 point object with x and y coordinates
+ * @param {Object} point2 point object with x and y coordinates
+ *
+ * @return the cross product result as a float
+ */
+function crossProduct(point1, point2) {
+	return point1.x * point2.y - point1.y * point2.x;
+}
+
+/**
+ * Subtract the second point from the first.
+ *
+ * @param {Object} point1 point object with x and y coordinates
+ * @param {Object} point2 point object with x and y coordinates
+ *
+ * @return the subtraction result as a point object
+ */
+function subtractPoints(point1, point2) {
+	var result = {};
+	result.x = point1.x - point2.x;
+	result.y = point1.y - point2.y;
+
+	return result;
+}
+
+/**
+ * See if the points are equal.
+ *
+ * @param {Object} point1 point object with x and y coordinates
+ * @param {Object} point2 point object with x and y coordinates
+ *
+ * @return if the points are equal
+ */
+function equalPoints(point1, point2) {
+	return (point1.x == point2.x) && (point1.y == point2.y)
+}
+
+/**
+ * See if all arguments are equal.
+ *
+ * @param {...} args arguments that will be compared by '=='.
+ *
+ * @return if all arguments are equal
+ */
+function allEqual(args) {
+	var firstValue = arguments[0],
+		i;
+	for (i = 1; i < arguments.length; i += 1) {
+		if (arguments[i] != firstValue) {
+			return false;
+		}
+	}
+	return true;
+}
